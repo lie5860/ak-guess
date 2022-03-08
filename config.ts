7fa5b2c6e5f9b6cea50c9b67ad3fd2b211c515ce
刘智煌 <zhihuang_liu@fzzixun.com>
2022-03-08 10:18:54 +0800

    调整接口参数

--- config.ts
+++ config.ts
@@ -1,5 +1,11 @@
 import {getDailyData, saveNum} from "./src/server";
 
 const afterDealData = ({chartsData}) => {
+  getDailyData().then(({num}) => {
+    if (num !== chartsData.length) {
+      saveNum(chartsData.length).then(() => {
+      })
+    }
+  })
 }
 export {afterDealData}
