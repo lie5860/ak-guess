713b3e25810e664da2f9ca01ef1afe7188c42306
lindsayee <lindsay89@foxmail.com>
2022-03-17 20:08:12 +0800

    修正非假名没法匹配的bug

--- src/utils/autocomplete.js
+++ src/utils/autocomplete.js
@@ -12,7 +12,7 @@ export default function autocomplete(inp, arr, chartsData, aliasData) {
             return false;
         }
         // 平假名转换成片假名去匹配结果
-        inputVal = inputVal.replace(/[u3041-\u3096]/g, function(match) {
+        inputVal = inputVal.replace(/[\u3041-\u3096]/g, function(match) {
             var chr = match.charCodeAt(0) + 0x60;
             return String.fromCharCode(chr);
         });
