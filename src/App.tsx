37279c8af139d8d16a879865b2104c9143297303
lindsayee <lindsay89@foxmail.com>
2022-03-17 20:27:06 +0800

    修复每日挑战显示放弃按钮的问题

--- src/App.tsx
+++ src/App.tsx
@@ -167,7 +167,7 @@ export default function Home() {
           setRandomAnswerKey(Math.floor(Math.random() * chartsData.length))
         }}>▶️ {i18n.get('newGameTip')}</a>
         }
-        {mode !== 'day' && !isOver && data?.length > 0 && <a className={'togglec'} onClick={() => {
+        {mode !== DAILY_MODE && !isOver && data?.length > 0 && <a className={'togglec'} onClick={() => {
           giveUp()
         }}>🆘 {i18n.get('giveUpTip')}</a>
         }
