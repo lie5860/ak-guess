a7d0f58f21cde00f8d2d0bb48e85e1c00c5aae00
刘智煌 <lie58600@gmail.com>
2022-04-12 18:50:46 +0800

    统计报告读取逻辑补充

--- src/component/History.tsx
+++ src/component/History.tsx
@@ -1,49 +1,84 @@
 import {React} from "../global";
 import ShareIcon from './ShareIcon'
 import copyCurrentDay from "../utils/copyCurrentDay";
-import {DAILY_MODE} from "../const";
+import {DAILY_MODE, modeI18nKeyDict, PARADOX_MODE, RANDOM_MODE} from "../const";
 import {AppCtx} from '../locales/AppCtx';
 import {localStorageGet, localStorageSet} from "../locales/I18nWrap";
 import {hostDict} from "../locales";
 
+export const DEFAULT_RECODE = {
+  playTimes: 0,
+  winTimes: 0,
+  totalTryTimes: 0,
+  winTryTimes: 0,
+  straightWins: 0,
+  maxStraightWins: 0,
+  dailyPlayTimes: 0,
+  dailyWinTimes: 0,
+  dailyWinTryTimes: 0,
+  dailyTotalTryTimes: 0,
+  dailyStraightWins: 0,
+  dailyMaxStraightWins: 0,
+  paradoxPlayTimes: 0,
+  paradoxWinTimes: 0,
+  paradoxWinTryTimes: 0,
+  paradoxTotalTryTimes: 0,
+  paradoxStraightWins: 0,
+  paradoxMaxStraightWins: 0
+}
 const loadRecordData = (lang: string) => {
   let record: any = localStorageGet(lang, "record");
   if (record) {
     record = JSON.parse(record);
   } else {
-    record = {
-      playTimes: 0,
-      winTimes: 0,
-      totalTryTimes: 0,
-      winTryTimes: 0,
-      straightWins: 0,
-      maxStraightWins: 0,
-      dailyPlayTimes: 0,
-      dailyWinTimes: 0,
-      dailyWinTryTimes: 0,
-      dailyTotalTryTimes: 0,
-      dailyStraightWins: 0,
-      dailyMaxStraightWins: 0
-    }
+    record = DEFAULT_RECODE
   }
   return record;
 }
 
 const saveRecordData = (lang: string, record: any) => {
-  localStorageSet(lang, "record", JSON.stringify(record));
+  localStorageSet(lang, "record", JSON.stringify({...DEFAULT_RECODE, ...record}));
 }
+const getDataFn = (record: any, mode: string) => {
+  switch (mode) {
+    case DAILY_MODE:
+      return {
+        playTimes: record?.dailyPlayTimes,
+        winTimes: record?.dailyWinTimes,
+        winPercent: record?.dailyPlayTimes && Math.ceil(record?.dailyWinTimes / record?.dailyPlayTimes * 100),
+        straightWins: record?.dailyStraightWins,
+        maxStraightWins: record?.dailyMaxStraightWins,
+        avgTryTimes: record?.dailyWinTimes && Math.ceil(record?.dailyWinTryTimes / record?.dailyWinTimes)
+      }
+    case RANDOM_MODE:
+      return {
+        playTimes: record?.playTimes,
+        winTimes: record?.winTimes,
+        winPercent: record?.playTimes && Math.ceil(record?.winTimes / record?.playTimes * 100),
+        straightWins: record?.straightWins,
+        maxStraightWins: record?.maxStraightWins,
+        avgTryTimes: record?.winTimes && Math.ceil(record?.winTryTimes / record?.winTimes)
+      }
 
+    case PARADOX_MODE:
+      return {
+        playTimes: record?.paradoxPlayTimes,
+        winTimes: record?.paradoxWinTimes,
+        winPercent: record?.paradoxPlayTimes && Math.ceil(record?.paradoxWinTimes / record?.paradoxPlayTimes * 100),
+        straightWins: record?.paradoxStraightWins,
+        maxStraightWins: record?.paradoxMaxStraightWins,
+        avgTryTimes: record?.paradoxWinTimes && Math.ceil(record?.paradoxWinTryTimes / record?.paradoxWinTimes)
+      }
+  }
+  return {}
+}
 const History = () => {
   const {i18n} = React.useContext(AppCtx);
   const record = loadRecordData(i18n.language);
   const getShareHistoryText = (mode: string) => {
-    const title = mode === DAILY_MODE ? i18n.get('dailyMode') : i18n.get('randomMode');
-    const playTimes = (mode === DAILY_MODE ? record?.dailyPlayTimes : record?.playTimes);
-    const winTimes = (mode === DAILY_MODE ? record?.dailyWinTimes : record?.winTimes);
-    const winPercent = (mode === DAILY_MODE ? record?.dailyPlayTimes && Math.ceil(record?.dailyWinTimes / record?.dailyPlayTimes * 100) : record?.playTimes && Math.ceil(record?.winTimes / record?.playTimes * 100));
-    const straightWins = (mode === DAILY_MODE ? record?.dailyStraightWins : record?.straightWins);
-    const maxStraightWins = (mode === DAILY_MODE ? record?.dailyMaxStraightWins : record?.maxStraightWins);
-    const avgTryTimes = (mode === DAILY_MODE ? record?.dailyWinTimes && Math.ceil(record?.dailyWinTryTimes / record?.dailyWinTimes) : record?.winTimes && Math.ceil(record?.winTryTimes / record?.winTimes));
+    console.log(mode, 'mode')
+    const title = i18n.get(modeI18nKeyDict[mode]);
+    const {playTimes, winTimes, winPercent, straightWins, maxStraightWins, avgTryTimes} = getDataFn(record, mode)
     let text = i18n.get('title') + ` ` + title + `\n`;
     text += i18n.get('playTimes') + playTimes + `\n`
     text += i18n.get('winTimes') + winTimes + `\n`
@@ -57,7 +92,7 @@ const History = () => {
   return <>
     <p className={'flex-center'}>
       <ShareIcon onClick={() => {
-        copyCurrentDay(getShareHistoryText('random'), i18n.get('copySuccess'))
+        copyCurrentDay(getShareHistoryText(RANDOM_MODE), i18n.get('copySuccess'))
       }}/>
       <span className='title'>
         {i18n.get('randomMode')}
